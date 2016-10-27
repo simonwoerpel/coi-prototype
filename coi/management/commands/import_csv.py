3916be92d8ec65b0241b4c44cb74b2eb9b6a008f
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-10-27 02:41:15 +0200

    Management: make `import_csv` work for every model

--- coi/management/commands/import_csv.py
+++ coi/management/commands/import_csv.py
@@ -1,22 +1,26 @@
 from csv import DictReader
 
+from django.apps import apps
 from django.core.management.base import BaseCommand, CommandError
 
-from ...models import Record
 
 
 class Command(BaseCommand):
-    help = 'Import given csv file into `Record`'
+    help = 'Import given csv file into given model'
 
     def add_arguments(self, parser):
+        parser.add_argument('model', type=str)
         parser.add_argument('csv_file', type=str)
 
     def handle(self, *args, **options):
+        Model = apps.get_model(options['model'])
+
         with open(options['csv_file']) as f:
             records = list(DictReader(f))
 
-        Record.objects.bulk_create(
-            [Record(**data) for data in records]
+        Model.objects.bulk_create(
+            [Model(**data) for data in records]
         )
 
-        self.stdout.write(self.style.SUCCESS('Successfully imported "%s" records.' % len(records)))
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully imported "%s" records into "%s".' % (len(records), options['model'])))
