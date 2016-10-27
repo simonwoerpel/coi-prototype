dbe112ed39819ce94912cd48c073429fe3c953bf
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-10-27 15:24:41 +0200

    Data: add paper urls

--- coi/models.py
+++ coi/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Record(models.Model):
     author_name = models.CharField(max_length=255)
     author_affiliation = models.CharField(max_length=2000, blank=True, null=True)
+    journal = models.SlugField()
     paper_date = models.DateField()
     paper_title = models.CharField(max_length=2000)
+    paper_url = models.URLField()
     conflict = models.TextField()
